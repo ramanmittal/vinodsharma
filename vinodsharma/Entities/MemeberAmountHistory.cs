4ade7f48ffacf45d0f7d68c8ab737f34c6d67202
ramanmittal <raman.mittal@kiwitech.com>
2018-02-17 13:01:24 +0530

    add amount

---
+++ vinodsharma/Entities/MemeberAmountHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vinodsharma.Entities
+{
+    public class MemeberAmountHistory
+    {
+        public int MemeberAmountHistoryID { get; set; }
+        public int MemeberID { get; set; }
+        public decimal AmountGiven { get; set; }
+        public DateTime Date { get; set; }
+        public string Remarks { get; set; }
+        public virtual Member Member { get; set; }
+    }
+}
